6b820dd8b880063d84f72a901d6f110c8b228ff3
LQ85i <39389364+LQ85i@users.noreply.github.com>
2024-01-07 03:04:13 +0200

    Add basic front-end structure

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET categories page. */
 router.get('/', function(req, res, next) {
-  res.render('categories', { title: 'Express' });
+  res.render('layout', { title: 'Express' });
 });
 
 module.exports = router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './views/**/*.pug',
+    './public/**/*.js',
+  ],
   theme: {
     extend: {},
   },
