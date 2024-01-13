d205aca5e16534dc6c05849bfd8878b6e0987e37
LQ85i <39389364+LQ85i@users.noreply.github.com>
2024-01-13 19:17:29 +0200

    Add forms for new items/categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -15,8 +15,44 @@ exports.render_categories = asyncHandler(async (req, res, next) => {
 
 exports.render_items = asyncHandler(async (req, res, next) => {
     itemData = await Item.find({}).exec();
-    const categoryId = req.params.objectId;
-    const currentCategory = await Category.findById(categoryId).exec();
+    const categoryID = req.params.objectId;
+    const currentCategory = await Category.findById(categoryID).exec();
     const page = "items"
     res.render('layout', { title: 'Items', currentCategory: currentCategory, items: itemData, page: page});
+});
+
+exports.render_new_category = asyncHandler(async (req, res, next) => {
+    const page = "new-category"
+    res.render('layout', { title: 'New Category', page: page});
+});
+
+exports.render_new_item = asyncHandler(async (req, res, next) => {
+    const page = "new-item"
+    categoryData = await Category.find({}).exec();
+    res.render('layout', { title: 'New Item', page: page, categories: categoryData});
+});
+
+exports.add_category = asyncHandler(async (req, res, next) => {
+    const name = req.body.categoryName;
+    const description = req.body.categoryDescription;
+    const URL = req.body.categoryURL;
+    await Category.create({ name: name, description: description, URL: URL });
+    res.redirect("/categories");
+});
+exports.add_item = asyncHandler(async (req, res, next) => {
+    const name = req.body.itemName;
+    const description = req.body.itemDescription;
+    const price = req.body.itemPrice;
+    const stock = req.body.itemStock;
+    const URL = req.body.itemURL;
+    const category = req.body.itemCategory;
+    await Item.create({ 
+        name: name, 
+        description: description, 
+        price: price, 
+        stock: stock, 
+        URL: URL,
+        category: category
+     });
+    res.redirect("/categories");
 });
--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,10 @@ var router = express.Router();
 const category_controller = require("../controllers/categoryController");
 
 router.get('/', category_controller.render_categories);
+router.get('/new-category', category_controller.render_new_category);
+router.get('/new-item', category_controller.render_new_item);
+router.post('/add-category', category_controller.add_category)
+router.post('/add-item', category_controller.add_item)
 router.get('/:objectId', category_controller.render_items);
 
 module.exports = router;
