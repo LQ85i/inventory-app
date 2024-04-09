561f428b61c6652c3afda0b351b1b05ac09ed36f
LQ85i <39389364+LQ85i@users.noreply.github.com>
2024-04-09 21:36:40 +0300

    Add authentication front-end.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,3 +4,21 @@ exports.render_index = asyncHandler(async (req, res, next) => {
     const page = "index"
     res.render('index', { title: 'Inventory Management App', page: page});
 });
+exports.render_landing = asyncHandler(async (req, res, next) => {
+    const page = "sign-in"
+    res.render('landing', { title: 'Inventory Management App', page: page});
+});
+
+exports.render_create_account = asyncHandler(async (req, res, next) => {
+    const page = "create-account"
+    res.render('landing', { title: 'Inventory Management App', page: page});
+});
+
+exports.sign_in = asyncHandler(async (req, res, next) => {
+    res.redirect("/categories");
+
+});
+
+exports.create_account = asyncHandler(async (req, res, next) => {
+    res.redirect("/categories");
+});
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const index_controller = require("../controllers/indexController");
 
-router.get('/', index_controller.render_index);
+router.get('/', index_controller.render_landing);
+router.get('/create-account', index_controller.render_create_account);
+router.post('/sign-in', index_controller.sign_in);
+router.post('/create-account', index_controller.create_account);
 
 module.exports = router;
